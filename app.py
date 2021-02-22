ff7d62ca41bf8e7763d47cd608076e6cc559eacf
ivekhov <ivekhov@gmail.com>
2021-02-22 17:02:06 +0300

    Add part of project week 03.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 
 WEEKDAYS = {
@@ -12,6 +12,7 @@ WEEKDAYS = {
 }
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def main():
@@ -27,12 +28,11 @@ def show_all_tutors():
 def find_goal(goal):
     return render_template("goal.html")
 
-# ToDo
+
 @app.route('/profiles/<profile_id>')
 def show_tutor_profile(profile_id):
     with open('data_teachers.json', 'r') as data_file:
         teachers = json.load(data_file)
-
     return render_template("profile.html", data=teachers[int(profile_id)],
                            weekdays=WEEKDAYS, profile_id=profile_id)
 
@@ -49,10 +49,32 @@ def confirm_request():
 
 @app.route('/booking/<profile_id>/<weekday>/<time>/')
 def reserve_tutor(profile_id, weekday, time):
-    return render_template("booking.html")
+    with open('data_teachers.json', 'r') as data_file:
+        teachers = json.load(data_file)
 
-@app.route('/booking_done/')
-def confirm_reservation():
+    # client_name = request.form.get["clientName"]
+    # client_phone = request.form.get["clientPhone"]
+    # client_time = request.form.get["clientTime"]
+    # client_weekday = request.form.get["clientWeekday"]
+
+    # visit = {
+    #     'client_name': client_name,
+    #     'client_phone': client_phone,
+    #     'weekday': client_weekday,
+    #     'time': client_time,
+    # }
+    # with open('booking.json', 'a') as data_file:
+    #     json.dump(visit, data_file)
+
+
+    return render_template("booking.html", data=teachers[int(profile_id)],
+                           weekdays=WEEKDAYS, profile_id=profile_id,
+                           weekday=weekday, time=time)
+
+
+@app.route('/booking_done/<clientName>')
+def confirm_reservation(clientName):
+    print(clientName)
     return render_template("booking_done.html")
 
 
